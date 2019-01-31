d180eb617b3e540004f5590e5fbe4857070a2561
WilliamYXN <williamyxn@gmail.com>
2019-01-31 22:29:20 +0800

    Changed some headers

--- SuperSnake/headers.h
+++ SuperSnake/headers.h
@@ -1,36 +1,31 @@
 #include "pch.h"
 #pragma once
-#include <iostream>
-#include <Windows.h>
-#include <ctime>
-#include <cstring>
-#include <string>
-#include <thread>
 
-constexpr short WHITE = 0;
-constexpr short L_WHITE = 1;
-constexpr short GREY = 2;
-constexpr short RED = 3;
-constexpr short PINK = 4;
-constexpr short BLUE = 5;
-constexpr short L_BLUE = 6;
-constexpr short GREEN = 7;
-constexpr short L_GREEN = 8;
-constexpr short GOLDEN = 9;
-constexpr short YELLOW = 10;
-constexpr short PURPLE = 11;
-constexpr short INDIGO = 12;
-constexpr short CYAN = 13;
-constexpr short CYANINE = 14;
+#include <Windows.h>
+#include <thread>
+#include <mutex> 
+#include "Tools.h"
+
+extern std::mutex mu;
+
+constexpr short WHITE = (short)0;
+constexpr short L_WHITE = (short)1;
+constexpr short GREY = (short)2;
+constexpr short RED = (short)3;
+constexpr short PINK = (short)4;
+constexpr short BLUE = (short)5;
+constexpr short L_BLUE = (short)6;
+constexpr short GREEN = (short)7;
+constexpr short L_GREEN = (short)8;
+constexpr short GOLDEN = (short)9;
+constexpr short YELLOW = (short)10;
+constexpr short PURPLE = (short)11;
+constexpr short INDIGO = (short)12;
+constexpr short CYAN = (short)13;
+constexpr short CYANINE = (short)14;
 
 #define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0)
 
-class Tools {
-	Tools();
-	~Tools();
-public:
-	void hideMouse();
-	int Rand(int x);
-	void gotoxy(short x, short y);
-	void coloredOutput(std::string st, short id);
-};
+int main();
+void theBeginning(Tools Tool);
+void pogress_bar();
