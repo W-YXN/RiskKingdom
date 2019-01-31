59f750430eb8165303b3a8a8cfdc6de27d1e6a03
WilliamYXN <williamyxn@gmail.com>
2019-01-31 22:28:46 +0800

    Added OP-Video

--- SuperSnake/SuperSnake.cpp
+++ SuperSnake/SuperSnake.cpp
@@ -1,12 +1,26 @@
-﻿// SuperSnake.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include "headers.h"
+
+std::mutex mu;
 
 int main()
 {
-	
+	SetConsoleTitle(TEXT("Snake"));
+	system("mode con cols=80 lines=31");
+	Tools *StartTool = new Tools;
+	mu.lock();
+	std::thread beginning{ theBeginning,*StartTool};
+	if (beginning.joinable()) {
+		std::cout << "threadID=" << beginning.get_id() << std::endl;
+		Sleep(4000);
+		system("cls");
+		mu.unlock();
+		if (beginning.joinable()) beginning.join();
+	}
+	mu.try_lock();
+	mu.unlock();
+	StartTool = NULL;
+	delete StartTool;
+	system("Pause");
 	return 0;
 }
-
